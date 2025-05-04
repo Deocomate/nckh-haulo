82ca72314047cd84f2255b62cb0a95ab4a216ae7
Deocomate <deocomate@gmail.com>
2025-05-04 10:11:28 +0700

    update

--- src/main/java/com/hau/websocket/entity/LoaiNguoiDung.java
+++ src/main/java/com/hau/websocket/entity/LoaiNguoiDung.java
@@ -5,7 +5,5 @@ package com.hau.websocket.entity;
  * Enum định nghĩa các loại người dùng trong hệ thống.
  */
 public enum LoaiNguoiDung {
-    SINH_VIEN,      // Sinh viên
-    CAN_BO_LOP,     // Cán bộ lớp
-    CAN_BO_PHONG_BAN // Cán bộ phòng ban
+    SINH_VIEN, CAN_BO_LOP, CAN_BO_PHONG_BAN
 }
